330548fd40b0faa229fc59704f49f698978ff001
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-09-10 16:45:11 +0200

    guessing game algorithm

--- Projects/Guessing game/app.js
+++ Projects/Guessing game/app.js
@@ -1,1 +1,35 @@
-console.log("Hello")
+const msg = "Welcome! Enter your max number:";
+
+let input = null;
+let guess = null;
+let attempts = 1;
+
+while(!input){
+    input = parseInt(prompt(msg));
+}
+
+// console.log(`Max number is ${input}`)
+
+const hiddenNumber = Math.round(Math.random() * input);
+
+guess = parseInt(prompt("Enter your first guess:"))
+
+while(guess !== hiddenNumber){
+    let hint = "";
+    if(guess > hiddenNumber){
+        hint = "Too high!"
+    }
+    else if(guess < hiddenNumber){
+        hint = "Too low!"
+    }
+
+    guess = parseInt(prompt(`${hint} Enter a new guess:`))
+    attempts++;
+}
+
+console.log("You got it!")
+console.log(`Hidden number was ${hiddenNumber}. It takes you ${attempts} attempts`)
+
+// console.log(`Hidden number is ${hiddenNumber}`)
+
+
