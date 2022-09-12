d2b2ebe9184d36724b3e722216e4d8c718789951
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-09-12 21:00:25 +0200

    command input processing

--- Projects/Console ToDo list/app.js
+++ Projects/Console ToDo list/app.js
@@ -1,1 +1,12 @@
-console.log("Connected")
+const chooseActionMsg = "What would you like to do?";
+
+const actions = ["new", "list", "delete", "quit"];
+
+let action = null;
+let todo = null;
+
+while(actions.indexOf(action) == -1){
+    action = prompt(chooseActionMsg);
+}
+
+console.log(`Your command is ${action}`);
