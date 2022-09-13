fad5272ab4cf792034114580b81c8d78d98cefb4
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-09-13 12:17:43 +0200

    commands processing

--- Projects/Console ToDo list/app.js
+++ Projects/Console ToDo list/app.js
@@ -1,12 +1,40 @@
 const chooseActionMsg = "What would you like to do?";
 
 const actions = ["new", "list", "delete", "quit"];
+const todoList = [];
 
 let action = null;
-let todo = null;
+let newTodo = null;
+let isQuit = false;
 
-while(actions.indexOf(action) == -1){
+while(!isQuit){
     action = prompt(chooseActionMsg);
+
+    switch(action) {
+        case "new":{
+            newTodo = prompt("What is the new todo?");
+            todoList.push(newTodo);
+            console.log(`${newTodo} added to the list`);
+        }break;
+
+        case "list":{
+            console.log("*****************");
+            for(let i = 0; i < todoList.length; i++){
+                console.log(`${i}: ${todoList[i]}`);
+            }
+            console.log("*****************");
+        }break;
+
+        case "delete":{
+            let index = prompt("Enter an index to delete:");
+            let deleted = todoList.splice(index, 1);
+            console.log(`Deleted ${deleted}`);
+        }break;
+
+        case "quit":{
+            isQuit = true;
+        }break;
+    }
 }
 
-console.log(`Your command is ${action}`);
+console.log("Quit");
